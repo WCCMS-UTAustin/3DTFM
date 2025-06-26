70957f30c351fc86474af9148c67bdc68cadbfaf
Gabriel Peery <peerygabriel@gmail.com>
2025-06-26 15:13:20 -0500

    Documented DoF restriction

--- gel/fix_dofs.py
+++ gel/fix_dofs.py
@@ -1,9 +1,23 @@
-"""Credit https://fenicsproject.discourse.group/t/specific-dof-of-a-function-as-control-variable/3300/3"""
+"""Base implementation of the function to restrict the control variables
+of the inverse model to only those in the event horizon.
+
+Generalized usage is also possible, this function will be overriden in
+`gel.fix_dofs_overloaded` with appropriate adjoint information for use
+with automatic differentiation.
+
+Credit https://fenicsproject.discourse.group/t/specific-dof-of-a-function-as-control-variable/3300/3"""
 from fenics import *
 from fenics_adjoint import *
 
 
 def fix_dofs(func, fixed_indexes, fixed_values):             
+    """Forces DoF values of a FE function to specific values.
+
+    * `func` is a FEniCS function in a space with DoFs
+    * `fixed_indexes` is an iterable of integer local DoF indices
+    * `fixed_values` is an iterable of the same size as `fixed_indexes`
+    with the float values to set the DoFs to.
+    """
     new_fun = Function(func.function_space(), func.vector())
     for idx, val in zip(fixed_indexes, fixed_values):
         new_fun.vector().vec().setValueLocal(idx, val)
--- gel/fix_dofs_overloaded.py
+++ gel/fix_dofs_overloaded.py
@@ -1,4 +1,9 @@
-"""Credit https://fenicsproject.discourse.group/t/specific-dof-of-a-function-as-control-variable/3300/3"""
+"""Overriden implementation of control variable restriction function for
+automatic differentiation in pyadjoint.
+
+Overrides `gel.fix_dofs.fix_dofs` with a Block object.
+
+Credit https://fenicsproject.discourse.group/t/specific-dof-of-a-function-as-control-variable/3300/3"""
 from fenics import *
 from fenics_adjoint import *
 from pyadjoint import Block
@@ -7,10 +12,16 @@ from pyadjoint.overloaded_function import overload_function
 from .fix_dofs import fix_dofs
 
 
-backend_fix_dofs = fix_dofs
+_backend_fix_dofs = fix_dofs
 
 class FixedDofsBlock(Block):
+    """Tape Block for `gel.fix_dofs.fix_dofs`
+
+    Describes how to recompute and how to compute the adjoint.
+    """
+
     def __init__(self, func, fixed_indexes, fixed_values, **kwargs):
+        """Take in arguments to the function and kwargs for adjoint."""
         super(FixedDofsBlock, self).__init__() 
         self.kwargs = kwargs
         self.add_dependency(func)
@@ -30,6 +41,16 @@ class FixedDofsBlock(Block):
             idx,
             prepared=None
         ):
+        """Zero-out the components of fixed DoFs for derivative.
+
+        * `inputs` is the list of inputs to `gel.fix_dofs.fix_dofs`, ie.
+        index 0 has the function.
+        * `adj_inputs` is a list with the same number of items as
+        `inputs`, has the values of the adjoint input
+        * Other inputs unused.
+
+        Returns: the resulting product accounting for fixed DoFs
+        """
         adj_input = adj_inputs[0] 
         x = inputs[0].vector() 
 
@@ -39,11 +60,28 @@ class FixedDofsBlock(Block):
         return output
     
     def recompute_component(self, inputs, block_variable, idx, prepared):
-        return backend_fix_dofs(
+        """Re-fixes the DoFs
+
+        * `inputs` is the list of inputs to `fix_dofs` originally 
+        encountered
+        * Rest of inputs unused
+
+        Returns: the DoF-fixed function.
+        """
+        return _backend_fix_dofs(
             inputs[0],
             self.fixed_indexes,
             self.fixed_values
         ) 
 
 fix_dofs = overload_function(fix_dofs, FixedDofsBlock)
+"""Forces DoF values of a FE function to specific values.
+
+Is equipped for automatic differentiation.
+
+* `func` is a FEniCS function in a space with DoFs
+* `fixed_indexes` is an iterable of integer local DoF indices
+* `fixed_values` is an iterable of the same size as `fixed_indexes`
+with the float values to set the DoFs to.
+"""
 
