21e99c27335c612d9362eb2cbe648036cc7fb5e1
Gabriel Peery <peerygabriel@gmail.com>
2025-06-26 15:14:06 -0500

    Refactored gel.py and documented

--- gel/gel.py
+++ gel/gel.py
@@ -1,3 +1,4 @@
+"""Interface to forward solving functionality and file management."""
 from .header import *
 
 from inspect import signature
@@ -12,7 +13,11 @@ from .objective import *
 
 _cell_name_cache = dict()
 def read_cell_name(cell_data_dir):
-    """Reads a cell name from a directory with caching."""
+    """Returns str name from directory str `cell_data_dir` with caching.
+
+    Will look for a file "name.txt" under provided directory and read
+    the first line.
+    """
     ret = None # Init
 
     if rank == 0:
@@ -34,7 +39,11 @@ def read_cell_name(cell_data_dir):
 
 _geo_type_cache = dict()
 def read_geo_type(cell_data_dir):
-    """Reads a geometry type from a directory with caching."""
+    """Returns str geometry type from directory str `cell_data_dir` w/ caching.
+
+    Will look for file "geo_type.txt" under provided directory and read
+    the first line.
+    """
     ret = None # Init
 
     if rank == 0:
@@ -56,9 +65,18 @@ def read_geo_type(cell_data_dir):
 
 def apply_fixes(
         geo,
-        mod_repr,
+        ctl,
         fillval=0.0
     ):
+    """Fixes DoFs beyond the event horizon to a fixed value.
+
+    Uses the subdomain data in `geo` (an object of type
+    `gel.geometry.Geometry`) to set DoFs of FEniCS function `ctl` to a
+    uniform value of `fillval` (default 0.0) in a new function.
+
+    Returns a new, fixed version of `ctl`, the modulus representation,
+    with adjoint tracing.
+    """
     # Construct fixed indices first
     free_indices = set()
     dof_map = geo.V0.dofmap()
@@ -81,7 +99,7 @@ def apply_fixes(
     fix_indices = np.array(list(fix_indices))
 
     fixed_mod_repr = fix_dofs(
-        mod_repr,
+        ctl,
         fix_indices,
         fillval*np.ones(fix_indices.shape, dtype=float)
     )
@@ -90,13 +108,21 @@ def apply_fixes(
 
 
 def get_variational_stuff(kinematics, Pi):
-    """
-    Given the kinematics and expression for energy, returns forms for the
-    variational problem.
+    r"""Returns the right-, left-hand sides of the variational problem.
+
+    It is defined in here, as opposed to `gel.mechanics`, because the
+    operators needed to solve a nonlinear problem must be invented
+    according to the solve technique; they are not intrinsic to the
+    problem. We use a consistent-tangent Newton-Raphson solver, so we
+    need the linearized tangent operator and a right-hand-side.
+
+    * `kinematics`: object of type `gel.kinematics.Kinematics` through
+    which to differentiate with respect to displacements
+    * `Pi`: expression for energy, likely an output from `gel.mechanics`
 
     Returns:
-    * dPi
-    * ddPi
+    * First derivative $\frac{d\Pi}{d\mathbf{u}}$ (RHS)
+    * Second derivative $\frac{d^2\Pi}{d\mathbf{u}^2}$ (LHS)
     """
     geo = kinematics.geo
 
@@ -111,63 +137,6 @@ def get_variational_stuff(kinematics, Pi):
     return dPi, ddPi
 
 
-def run_forward(
-    kinematics,
-    dPi,
-    ddPi,
-    solver_parameters,
-    ffc_options,
-    load_steps=1,
-    post_load_step_callback=None
-):
-    """
-    Given the kinematics, expressions for the variational problem, and solver
-    settings (including how many load_steps to split into), performs the forward
-    solve.
-
-    May provide function to call with every chunk. Note it is called after each
-    chunk.
-
-    Main side effect is to modify kinematics.u with the solution.
-    """
-    geo = kinematics.geo
-
-    # Solver loop (solves the forward problem in load_steps)
-    for i in range(load_steps):
-        sys.stdout.flush()  
-
-        if hasattr(geo, "scalar"):
-            # updates the boundary conditions surface displacements
-            geo.scalar = (i+1)/load_steps
-            geo.update_bcs()
-
-        # Be careful of load stepping and adjoint annotation!
-        stop_tape = False
-        if i != load_steps-1:
-            stop_tape = True
-
-        if stop_tape:
-            pa.pause_annotation()
-
-        # Solve variational problem
-        nvp = NonlinearVariationalProblem(
-            dPi,
-            kinematics.u,
-            geo.bcs,
-            J=ddPi,
-            form_compiler_parameters=ffc_options
-        )
-        nvs = NonlinearVariationalSolver(nvp)
-        nvs.parameters.update(solver_parameters)
-        nvs.solve()
-
-        if stop_tape:
-            pa.continue_annotation()
-
-        if post_load_step_callback is not None:
-            post_load_step_callback()
-
-
 # There is no reason to record what happends here for inverse model
 @pa.tape.no_annotations
 def output_results_paraview(
@@ -177,17 +146,22 @@ def output_results_paraview(
         bx_inds=None,
         name="simulation_output"
     ):
-    """
-    Given kinematics with stuff having been computed, a directory to put the
-    results, and mod_repr as used, saves mesh with solution info.
+    """Outputs simulation state information as files in multiple forms.
 
-    If given a list of boundary indicators, will output that as well.
-
-    May override default name simulation_output with name
+    * `output_folder`: str path to directory where files will be created
+    * `kinematics`: `gel.kinematics.Kinematics` with displacements
+    * `mod_repr`: FEniCS function with control variable DoFs
+    * `bx_inds`: dict, if not None it enables writing boundary tags on
+    hydrogel mesh with keys the tags and items the names
+    * `name`: str the name of the nodal output file
 
     Output files:
-    * {name}.xdmf
-    * {name}.h5
+    * {name}.xdmf, .h5: nodal form with displacement, control variable,
+    rank ownership for DoFs, and optionally boundary conditions
+    * u_full_shape.xdmf, .h5: full shape/write_checkpoint form of
+    displacements for easy reloading back into FEniCS
+    * mod_repr_full_shape.xdmf, .h5: full shape/write_checkpoint form of
+    the control variables for easy reloading back into FEniCS
     """
     geo = kinematics.geo
 
@@ -234,11 +208,26 @@ def output_results_paraview(
 
 
 class ForwardSimulation:
+    """Minimal-prerequisite interface to running forward simulations.
+
+    Typical usage, an example of obtaining displacements from
+    homogeneous modulus:
+    ```
+    sim = ForwardSimulation("real_cell_gel", data_directory="cell_data")
+    sim.run_forward()
+    kinematics = sim.kinematics # Contains displacements u
+    ```
+
+    Many internal variables (below) are exposed for customization before
+    calls to sim.run_forward(). Be careful to keep pointers between the
+    `geo`, `kinematics`, and `mechanics` objects in sync by not
+    overwriting them or their depedency injections.
+    """
 
     def __init__(self,
             geometry_type,
             d1c1=1,
-            material_model="beta",
+            formulation="beta",
             mod_repr_init="zero",
             vprint=do_nothing_print,
             mu_ff=MU_FF_DEFAULT,
@@ -251,6 +240,38 @@ class ForwardSimulation:
             formulation_kwargs=dict(),
             **kwargs
         ):
+        r"""Constructor for object containing forward simulation state.
+
+        * `geometry_type`: str tag of what type of geometry (only
+        "real_cell_gel" -> `gel.geometry.Geometry` is currently
+        implemented)
+        * `d1c1`: float ratio $\frac{D_1}{c_1}$
+        * `formulation`: str tag of material model formulation
+        implemented in `gel.mechanics`
+        * `mod_repr_init`: (the initial value of) the control variable for
+        modulus; options in `gel.mechanics`
+        * `vprint`: callable function to perform action of print or
+        logging
+        * `mu_ff`: float far field rheometry measurement, ie.
+        $c_1=\frac{\mu}{2}$
+        * `load_steps`: int number of load steps to use in forward solve
+        (1 implies no stepping, just use the BC)
+        * `restrict_ctl_dofs_to_veh`: bool, enables fixing DoFs outside
+        event horizon for the inverse model
+        * `pc`: str name of preconditioner, ie. an option listed in
+        `list_krylov_solver_preconditioners()`
+        * `tola`: float absolute tolerance of Newton-Raphson
+        * `tolr`: float relative tolerance of Newton-Raphson
+        * `traction`: str or None. If not None, the filename of a
+        full-shape .xdmf traction function on the hydrogel mesh. Must
+        be in 1st order Lagrange space, will ignore DoFs outside of
+        surface.
+        * `formulation_kwargs`: dict of additional kwargs to the
+        material model formulation (if applicable) implemented in
+        `gel.mechanics`
+        * `kwargs`: dict of kwargs to the the geometry, for instance
+        the arguments to `gel.geometry.Geometry`
+        """
         # Validate, get geometry
         if traction is not None:
             # Update kwargs -> no Dirichlet BC allowed
@@ -260,31 +281,96 @@ class ForwardSimulation:
 
             kwargs["suppress_cell_bc"] = True
         self.geo = self._geo_from_kwargs(geometry_type, kwargs)
+        """Corresponding object of type `gel.geometry.Geometry`"""
 
-        self.traction = traction
-        if traction is not None:
-            self.traction = Function(self.geo.V)
-            t_file = XDMFFile(traction)
-            t_file.read_checkpoint(self.traction, "T", 0)
+        # Initialize displacements, kinematic quantities
+        self.kinematics = Kinematics(self.geo)
+        """Object of type `gel.kinematics.Kinematics` with displacement
+        information
+        """
 
-        self.d1c1 = d1c1
-        self.material_model = material_model
-        self.mod_repr_init = mod_repr_init
-        self.vprint = vprint
-        self.mu_ff = mu_ff
+        # Prepare modulus control variable
+        self.ctl = create_mod_repr(self.geo, mod_repr_init)
+        """A FEniCS function with the control variables for optimization,
+        *cf.* `mod_repr`
+        """
+
+        self.mod_repr = None
+        r"""The representation of modulus, for instance $\beta$, of type FEniCS
+        function. DoFs have already been fixed if necessary in this object.
+        *cf.* `ctl`.
+        """
+        if restrict_ctl_dofs_to_veh:
+            self.mod_repr = apply_fixes(
+                self.geo,
+                self.ctl,
+                fillval=(
+                    0.0 if (self.mechanics.formulation in ZERO_FIX_F) else 1.0
+                )
+            )
+        else:
+            self.mod_repr = self.ctl
+
+        # Prepare mechanics
+        self.mechanics = Mechanics(
+            self.kinematics,
+            formulation,
+            mu_ff,
+            d1c1,
+            self.mod_repr,
+            **formulation_kwargs
+        )
+        """Object of type `gel.mechanics.Mechanics` with material model
+        information. Has an internal `mechanics.kinematics` variable
+        that is the same as the ForwardSimulation's `kinematics`.
+        """
+
+        self.B = Constant((0.0, 0.0, 0.0))
+        """FEniCS body force per unit volume"""
+
+        self.T = None
+        """FEniCS traction force on boundary per area in reference"""
+        if traction is None:
+            self.T = Constant((0.0, 0.0, 0.0))
+        else:
+            self.T = load_shape(self.geo.V, traction, "T")
+
+        self.solver_parameters = {
+            "newton_solver" : {
+                "absolute_tolerance" : tola,
+                "relative_tolerance" : tolr,
+                "linear_solver" : "gmres",
+                "preconditioner" : pc,
+                "maximum_iterations" : 8,
+                "error_on_nonconvergence" : False,
+                #"krylov_solver" : {
+                    #"absolute_tolerance" : 0.1*self.tola,
+                    #"relative_tolerance" : 1e-4
+                #}
+            }
+        }
+        """dict of parameters set in `NonlinearVariationalSolver`"""
+
+        self.ffc_options = {
+            "optimize": True,
+            "eliminate_zeros": True,
+            "precompute_basis_const": True,
+            "precompute_ip_const": True
+        }
+        """dict of form compiler parameters set in
+        `NonlinearVariationalProblem`
+        """
+
         self.load_steps = load_steps
-        self.restrict_ctl_dofs_to_veh = restrict_ctl_dofs_to_veh
-        self.pc = pc
-        self.tola = tola
-        self.tolr = tolr
-        self.formulation_kwargs = formulation_kwargs
+        """Number of load steps in Dirichlet BCs during single solve"""
+
+        self.vprint = vprint
+        """Optional print/logging functionality"""
 
     def _geo_from_kwargs(self, geometry_type, kwargs):
         # Parse geometry
         if geometry_type == "real_cell_gel":
             geo_cls = Geometry
-        elif geometry_type == "beam":
-            geo_cls = BeamGeometry
         else:
             raise ValueError(f"{geometry_type} unknown")
 
@@ -316,113 +402,55 @@ class ForwardSimulation:
         return geo_cls(*cls_args_pos_only, **cls_args_kwarg)
 
     def run_forward(self):
-        # Get easier access
-        geo = self.geo
-        formulation = self.material_model
-        d1c1 = self.d1c1
-        mod_repr_init = self.mod_repr_init
-        vprint = self.vprint
+        r"""Solves the forward problem with saved settings.
 
-        # Do the normal things
-        kinematics = Kinematics(geo)
+        Side effects: 
+        * `kinematics` will store solved displacements
+        * `mechanics` will contained solved displacements through the
+        same `kinematics` instance
+        * boundary conditions in `geo` are changed if load stepping is
+        enabled
+        * Uses `vprint` for logging before solve
+        """
+        # Optionally print information
+        self.vprint(f"Using D1/C1={self.mechanics.d1c1}")
+        self.vprint(f"Using far field mu={self.mechanics.mu_ff} Pa")
+        self.vprint(f"solver_parameters = {self.solver_parameters}")
+        self.vprint(f"ffc_options = {self.ffc_options}")
 
-        # Define material
-        mu_ff = self.mu_ff
-        vprint(f"Using D1/C1={d1c1}")
-        nu = get_nu_for_target_k(d1c1)
-        vprint(f"Measured D1/C1 off of nu, found {get_k_for_target_nu(nu)}")
-        vprint(f"Using mu={mu_ff} Pa")
-         
-        B  = Constant((0.0, 0.0, 0.0))  # Body force per unit volume
-        # Traction force on the boundary per area in reference
-        if self.traction is None:
-            T = Constant((0.0, 0.0, 0.0))
-        else:
-            T = self.traction
+        # Assemble variational problem with current state
+        Pi = self.mechanics.get_energy(B=self.B, T=self.T)
+        dPi, ddPi = get_variational_stuff(self.kinematics, Pi)
 
-        if isinstance(mod_repr_init, str):
-            if mod_repr_init in MOD_REPR_FIELDS:
-                # Case lookup
-                self.ctl = MOD_REPR_FIELDS[mod_repr_init](geo)
-            else:
-                # Case read from file
-                self.ctl = Function(geo.V0)
+        # Solver loop (solves the forward problem in load_steps)
+        for i in range(self.load_steps):
+            sys.stdout.flush()  
 
-                mod_repr_file = XDMFFile(mod_repr_init)
-                mod_repr_file.read_checkpoint(self.ctl, "mod_repr", 0)
+            if hasattr(self.geo, "scalar"):
+                # updates the boundary conditions surface displacements
+                self.geo.scalar = (i+1)/self.load_steps
+                self.geo.update_bcs()
 
-                self.ctl.set_allow_extrapolation(True)
-        else:
-            # Case given the function
-            self.ctl = mod_repr_init
+            # Be careful of load stepping and adjoint annotation!
+            stop_tape = False
+            if i != load_steps-1:
+                stop_tape = True
 
-        self.ctl.rename("ctl", "ctl")
-        if self.restrict_ctl_dofs_to_veh:
-            mod_repr = apply_fixes(
-                geo,
-                self.ctl,
-                fillval=(0.0 if (formulation in ZERO_FIX_F) else 1.0)
+            if stop_tape:
+                pa.pause_annotation()
+
+            # Solve variational problem
+            nvp = NonlinearVariationalProblem(
+                dPi,
+                self.kinematics.u,
+                self.geo.bcs,
+                J=ddPi,
+                form_compiler_parameters=self.ffc_options
             )
-        else:
-            mod_repr = self.ctl
+            nvs = NonlinearVariationalSolver(nvp)
+            nvs.parameters.update(self.solver_parameters)
+            nvs.solve()
 
-        Pi = get_energy(
-            kinematics,
-            mu_ff,
-            nu,
-            mod_repr,
-            B,
-            T,
-            formulation=formulation,
-            **self.formulation_kwargs
-        )
-
-        # Solve
-        solver_parameters = {
-            "newton_solver" : {
-                "absolute_tolerance" : self.tola,
-                "relative_tolerance" : self.tolr,
-                "linear_solver" : "gmres",
-                "preconditioner" : self.pc,
-                "maximum_iterations" : 8,
-                "error_on_nonconvergence" : False,
-                #"krylov_solver" : {
-                    #"absolute_tolerance" : 0.1*self.tola,
-                    #"relative_tolerance" : 1e-4
-                #}
-            }
-        }
-
-        ffc_options = {
-            "optimize": True,
-            "eliminate_zeros": True,
-            "precompute_basis_const": True,
-            "precompute_ip_const": True
-        }
-
-        vprint(f"solver_parameters = {solver_parameters}")
-        vprint(f"ffc_options = {ffc_options}")
-
-        dPi, ddPi = get_variational_stuff(kinematics, Pi)
-        solver_fn = lambda : run_forward(
-            kinematics,
-            dPi,
-            ddPi,
-            solver_parameters,
-            ffc_options,
-            load_steps=self.load_steps
-        )
-        solver_fn()
-
-        # Save results
-        self.kinematics = kinematics
-        self.mod_repr = mod_repr
-        self.solver_fn = solver_fn
-        self.mechanics = Mechanics(
-            kinematics,
-            formulation,
-            mu_ff,
-            d1c1,
-            mod_repr
-        )
+            if stop_tape:
+                pa.continue_annotation()
 
