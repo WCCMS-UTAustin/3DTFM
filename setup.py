a3c611d4ac926132c42b807ea6dba5e72ee5846c
Gabriel Peery <peerygabriel@gmail.com>
2026-04-07 13:30:38 -0500

    Rename scripts to avoid conflicts

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts" : [
-            "forward = gel.scripts:forward",
-            "inverse = gel.scripts:inverse",
-            "downsample_mesh = gel.scripts:downsample_mesh_main",
-            "get_u = gel.scripts:get_u_main",
-            "get_kinematics_mesh = gel.scripts:get_kinematics_mesh",
-            "get_veh = gel.scripts:get_veh_main",
-            "get_displacement_from_gpr = gel.scripts:get_u_from_gpr_main",
-            "create_cell_surf_normals_mesh = gel.scripts:create_cell_surf_normals_mesh_main",
+            "forward_hydrogel = gel.scripts:forward",
+            "inverse_hydrogel = gel.scripts:inverse",
+            "downsample_mesh_hydrogel = gel.scripts:downsample_mesh_main",
+            "get_u_hydrogel = gel.scripts:get_u_main",
+            "get_kinematics_mesh_hydrogel = gel.scripts:get_kinematics_mesh",
+            "get_veh_hydrogel = gel.scripts:get_veh_main",
+            "get_displacement_from_gpr_hydrogel = gel.scripts:get_u_from_gpr_main",
+            "create_cell_surf_normals_mesh_hydrogel = gel.scripts:create_cell_surf_normals_mesh_main",
         ]
     }
 )
